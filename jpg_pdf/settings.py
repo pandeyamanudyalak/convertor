f1c181caa1eccb326c1569aba4f82f0d9080c844
aman <amanpandeyudyalak@gmail.com>
2024-09-29 21:04:02 +0530

    removed static

--- jpg_pdf/settings.py
+++ jpg_pdf/settings.py
@@ -129,7 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # settings.py
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'

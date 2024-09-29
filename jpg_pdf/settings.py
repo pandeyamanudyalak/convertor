92b8ca28c9a5446a699165efd63297291d0002fc
aman <amanpandeyudyalak@gmail.com>
2024-09-29 21:25:41 +0530

    remove static

--- jpg_pdf/settings.py
+++ jpg_pdf/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # settings.py
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')]
+
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
@@ -138,6 +138,7 @@ DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 # Media Setting
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

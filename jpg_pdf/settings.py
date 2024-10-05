e04a37a9d797a4aac18b3fa0d89d8e7888e69e8a
aman <amanpandeyudyalak@gmail.com>
2024-10-05 22:31:28 +0530

    setting updated

--- jpg_pdf/settings.py
+++ jpg_pdf/settings.py
@@ -144,6 +144,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 #     "/home/ubuntu/project/convertor/static",
 # ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
